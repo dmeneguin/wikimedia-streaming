6841fa7cdf7c87df320ba120672b0d125dbe6d85
dmeneguin <meneguin.daniel@gmail.com>
2025-04-28 00:38:52 -0300

    add: flow working from event to lake

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,10 +1,19 @@
 import json
 from requests_sse import EventSource
 from validation import validate_data
-import pymongo
+from kafka import KafkaProducer
 
-mongo_client = pymongo.MongoClient('mongodb://root:password@mongo:27017/')
 url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+# Configuration
+KAFKA_BROKER = 'kafka:29092'  # Adjust if needed
+TOPIC_NAME = 'delta-stream'
+
+# Create Kafka producer
+producer = KafkaProducer(
+    bootstrap_servers=[KAFKA_BROKER],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Serialize JSON
+)
+
 with EventSource(url) as stream:
     for event in stream:
         if event.type == 'message':
@@ -18,5 +27,12 @@ with EventSource(url) as stream:
                     continue            
 
                 validation = validate_data(change)
-                mongo_client.db.events.insert_one({'payload': change, 'valid': validation['result']})
 
+                # Example JSON data to send
+                data = {'payload': change, 'valid': validation['result']}
+                # Send JSON message
+                future = producer.send(TOPIC_NAME, value=data)
+                result = future.get(timeout=10)  # Block until a single message is sent (or timeout)
+                print(f"Message sent! Metadata: {result}")
+                producer.flush()
+producer.close()
